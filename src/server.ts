77a9c14475400851b1e5338a7b6eb8d09e1d79cb
Deusimar <deusimarmms@outlook.com>
2022-03-28 01:13:14 -0300

    servidor com as importações e configurações do projeto

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
-
+import mainRouts from './routes/index';
 dotenv.config();
 
 const server = express();
@@ -14,5 +14,10 @@ server.engine('mustache',mustache());
 server.use(express.static(path.join(__dirname,'../public')));
 
 /* Rotas */
+server.use(mainRouts);
+
+server.use((req,res)=>{
+  res.render('pages/404')
+})
 
 server.listen(process.env.PORT);
